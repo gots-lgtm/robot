fa0007dbdd9818c90aa04aea88b16b82f5aa5afe
gots-lgtm <78681830+gots-lgtm@users.noreply.github.com>
2021-02-07 12:40:44 +0300

    Add files via upload

---
+++ print(`Hello World`).py
@@ -0,0 +1,15 @@
+carrots = 50
+classmates = []
+# carrots = carrots - 10.0
+# print(carrots)
+# print('Hello World')
+# classmates.append('Sofia')
+# classmates.append('Arina')
+
+
+while carrots > 0:
+    print(carrots)
+    classmates.append(carrots)
+    carrots = carrots-1
+
+print(classmates)
